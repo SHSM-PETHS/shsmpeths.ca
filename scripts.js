6dc24e274e018938ff264d9baa20caec7a92ab8f
qiaoandrew <36504195+qiaoandrew@users.noreply.github.com>
2021-12-04 10:59:21 -0500

    Finish dropdown menu

--- scripts.js
+++ scripts.js
@@ -3,6 +3,9 @@ const background = document.querySelector('#menuBackground');
 const body = document.body;
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
+const dropdown = document.body.querySelector('.dropdown');
+const dropdownItems = document.body.querySelector('.dropdown-items');
+let dropdownOpen = false;
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open');
@@ -16,15 +19,33 @@ menuBtn.addEventListener('click', () => {
     background.classList.remove('open-menu-background');
     body.classList.remove('no-scrolling');
     menuOpen = false;
+    dropdownOpen = false;
+    dropdownItems.classList.remove('open-dropdown');
   }
 })
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth > 992 && menuOpen === true) {
-    menuBtn.classList.remove('open');
-    navItems.classList.remove('open-nav-items');
-    background.classList.remove('open-menu-background');
-    body.classList.remove('no-scrolling');
-    menuOpen = false;
+  if (window.innerWidth > 992) {
+    if (menuOpen === true) {
+      menuBtn.classList.remove('open');
+      navItems.classList.remove('open-nav-items');
+      background.classList.remove('open-menu-background');
+      body.classList.remove('no-scrolling');
+      menuOpen = false;
+    }
+    dropdownOpen = false;
+    dropdownItems.classList.remove('open-dropdown');
   }
+});
+
+document.addEventListener('click', function (e) {
+  if (!dropdownOpen && e.target.innerText == dropdown.innerText && window.innerWidth <= 992) {
+    dropdownItems.classList.add('open-dropdown');
+    dropdownOpen = true;
+  } else {
+    dropdownItems.classList.remove('open-dropdown');
+    console.log('removed');
+    dropdownOpen = false;
+  }
+  console.log(dropdownOpen);
 });
